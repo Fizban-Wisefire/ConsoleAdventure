ab8e48c095b91ff7e491e9f1a522d6e03f831055
PATRICK HOWARD <patrickallenhoward@gmail.com>
2024-02-17 07:23:45 -0500

    Got game fully playable and able to do nonstop combat.

--- ConsoleAdventure/Program.cs
+++ ConsoleAdventure/Program.cs
@@ -9,24 +9,66 @@ string input;
 
 Random random = new Random();
 
-//Makes the Player and all Monsters of the Character class.
 
-Character PlayerCharacter = new Character("Player", 5, 5, 5, 5, 5, 5);
+// Create Weapons, Armor, and Potions and adds usable items to lists
+
+Item Unarmed = new Item("Weapon", "Unarmed", 1, 0);
+Item Dagger = new Item("Weapon", "Dagger", 2, 5);
+Item ShortSword = new Item("Weapon", "Short Sword", 4, 10);
+Item LongSwort = new Item("Weapon", "Long Sword", 6, 15);
+Item Claymore = new Item("Weapon", "Claymore", 8, 20);
+
+List<Item> Weapons = new List<Item>();
+Weapons.Add(Dagger);
+Weapons.Add(ShortSword);
+Weapons.Add(LongSwort);
+Weapons.Add(Claymore);
+
+Item Unarmored = new Item("Armor", "Unarmored", 0, 0);
+Item Leather = new Item("Armor", "Leather", 2, 5);
+Item Chainmail = new Item("Armor", "Chainmail", 4, 10);
+Item PlateArmor = new Item("Armor", "Plate Armor", 6, 20); 
+
+List<Item> Armors = new List<Item>();
+Armors.Add(Leather);
+Armors.Add(Chainmail);
+Armors.Add(PlateArmor);
+
+
+Item SmallPotion = new Item("Potion", "Small Potion", 5, 3);
+Item MediumPotion = new Item("Potion", "Medium Potion", 10, 6);
+Item LargePotion = new Item("Potion", "Large Potion", 15, 9);
+
+List<Item> Potions = new List<Item>();
+Potions.Add(SmallPotion);
+Potions.Add(MediumPotion);
+Potions.Add(LargePotion);
+
+//Makes the Player and all Monsters of the Character class. And Create Players Inventory
+
+Character PlayerCharacter = new Character("Player", 5, 2, 2, 2, 2, 2, Unarmed, Unarmored);
+List<Item> PlayerInventory = new List<Item>();
+
+// Ints to store the amount of potions the player has
+
+int PlayerSmPotion = 0;
+int PlayerPotion = 0;
+int PlayerLgPotion = 0;
 
 //----------------------------------------------------------------------------|
-// Should I make a subclass for mobs??                                 *******|
+// Should I make a subclass for mobs?? Maybe add them in while creating mobs?  *******|
 //----------------------------------------------------------------------------|
 
-Character Goblin = new Character("Goblin", 1, 2, 5, 3, 3, 1);
-Character Orc = new Character("Orc", 2, 2, 5, 3, 3, 2);
-Character Ogre = new Character("Ogre", 3, 2, 5, 3, 3, 3);
-Character Bandit = new Character("Bandit", 4, 2, 5, 3, 3, 4);
-Character BanditCaptain = new Character("Bandit Captain", 5, 2, 5, 3, 3, 5);
-Character Dragon = new Character("Dragon", 6, 2, 5, 3, 3, 6);
+Character Goblin = new Character("Goblin", 1, 2, 5, 3, 3, 1, Unarmed, Unarmored);
+Character Orc = new Character("Orc", 2, 2, 5, 3, 3, 2, Unarmed, Unarmored);
+Character Ogre = new Character("Ogre", 3, 2, 5, 3, 3, 3, Unarmed, Unarmored);
+Character Bandit = new Character("Bandit", 4, 2, 5, 3, 3, 4, Unarmed, Unarmored);
+Character BanditCaptain = new Character("Bandit Captain", 5, 2, 5, 3, 3, 5, Unarmed, Unarmored);
+Character Dragon = new Character("Dragon", 6, 2, 5, 3, 3, 6, Unarmed, Unarmored);
 
 //Adds all of the Monsters into a Monsters List
 
-var Monsters = new List<Character>();
+List<Character> Monsters = new List<Character>();
 Monsters.Add(Goblin);
 Monsters.Add(Orc);
 Monsters.Add(Ogre);
@@ -34,24 +76,41 @@ Monsters.Add(Bandit);
 Monsters.Add(BanditCaptain);
 Monsters.Add(Dragon);
 
-//Where we will put all other methods of the game.
+// Creats all methods for the game loop
 
-//*****************************************************************************
-//----------------------------------------------------------------------------|
-// Having to find out how to make a copy of an item and not use original  ****| I THINK i need to make a dictionary for each monster then add all of the dictionarys to a list so that I can pull 1 at a time to make an object while in a loop.
-//----------------------------------------------------------------------------| If you make an object in a for loop it is destroyed when the loop is destroyed.
-//*****************************************************************************
-void Fight(Character Player, Character Foe)
+void GameOver()
 {
+    Console.WriteLine("!!!!!GAME OVER!!!!!");
+    game = false;
+}
+
+void Fight(Character Player, Character target)
+{
+
+    Character Foe = target.Clone();
     Console.WriteLine(Player.Name + " is fighting " + Foe.Name);
+
     while (PlayerCharacter.Hp > 0 && Foe.Hp > 0)
-    {
-        PlayerCharacter.Attack(Foe);
-        if (Foe.Hp > 0)
+    { 
+        Console.WriteLine("It is your turn, what would you like to do?");
+        Console.WriteLine("1) Attack 2) Bag 3) Run");
+        input = Console.ReadLine();
+        if (input == "1")
         {
+            PlayerCharacter.Attack(Foe);
+            if (Foe.Hp <= 0)
+            {
+                Console.WriteLine($"You have deafeted the {Foe.Name}!");
+                Console.WriteLine($"They dropped {Foe.Value} gold.");
+            }
+            Console.WriteLine("It is the foes turn. they attack!");
             Foe.Attack(Player);
+        } else if (input == "2")
+        {
+            Inventory();
         }
-    } if (PlayerCharacter.Hp > 0)
+    }  
+    if (PlayerCharacter.Hp > 0)
     {
         Console.WriteLine(Foe.Name + " is dead.");
         Player.Value += Foe.Value;
@@ -59,39 +118,185 @@ void Fight(Character Player, Character Foe)
     } else 
     {
         Console.WriteLine(PlayerCharacter.Name + " is dead.");
+        GameOver();
     }
 }
-
 void Shop()
 { 
     Console.WriteLine("This is the store");
     Console.WriteLine("You have " + PlayerCharacter.Value + " gold.");
-    Console.WriteLine("What would you like to buy? 1) Health Potion(3 gold) 2) Weapon Upgrade(5 gold)");
+    Console.WriteLine("What would you like to shop for? 1) Weapons 2) Armor 3) Potions");
     input = Console.ReadLine();
+
+    // Lists and Allows Player to buy weapons after checking if he has enough gold for the selected weapon
+
     if (input == "1")
     {
-        PlayerCharacter.Value -= 3;
-        PlayerCharacter.Hp += 5;
-        Console.WriteLine("You heal by 5, you now have " + PlayerCharacter.Hp + " HP.");
-        Console.WriteLine("You now have " + PlayerCharacter.Value + " gold.");
+        for (int i = 0; i < Weapons.Count; i += 1)
+        {
+            Item weapon = Weapons[i];
+            Console.WriteLine(weapon.Name + " (" + weapon.Value + " gold)");
+        }
+        Console.WriteLine("Would you like to buy any?");
+        Console.WriteLine("1) Dagger 2)Short Sword 3) Long Sword 4)Claymore");
+        input = Console.ReadLine();
+        if ((input == "1") && (PlayerCharacter.Value >= Weapons[0].Value))
+        {
+            PlayerCharacter.Weapon = Weapons[0];
+            PlayerCharacter.Value -= Weapons[0].Value;
+            Console.WriteLine("You have equiped a Dagger and have " + PlayerCharacter.Value + " gold left.");
+        } else if ((input == "2") && (PlayerCharacter.Value >= Weapons[1].Value))
+        {
+            PlayerCharacter.Weapon = Weapons[1];
+            PlayerCharacter.Value -= Weapons[1].Value;
+            Console.WriteLine("You have equiped a Short Sword and have " + PlayerCharacter.Value + " gold left.");
+        } else if ((input == "3") && (PlayerCharacter.Value >= Weapons[2].Value))
+        {
+            PlayerCharacter.Weapon = Weapons[2];
+            PlayerCharacter.Value -= Weapons[2].Value;
+            Console.WriteLine("You have equiped a Long Sword and have " + PlayerCharacter.Value + " gold left.");
+        } else if ((input == "4") && (PlayerCharacter.Value >= Weapons[3].Value))
+        {
+            PlayerCharacter.Weapon = Weapons[3];
+            PlayerCharacter.Value -= Weapons[3].Value;
+            Console.WriteLine("You have equiped a Claymore and have " + PlayerCharacter.Value + " gold left.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid option or not enough gold.");
+            Shop();
+        }
     }
+
+    // Lists and Allows Player to buy armor after checking if he has enough gold for the selected armor
+
     else if (input == "2")
     {
-        PlayerCharacter.Str += 1;
-        Console.WriteLine("Your attack went up by one and is now " + PlayerCharacter.Str + ".");
-        Console.WriteLine("You now have " + PlayerCharacter.Value + " gold.");
+        for (int i = 0; i < Armors.Count; i += 1)
+        {
+            Item armor = Armors[i];
+            Console.WriteLine(armor.Name + " (" + armor.Value + " gold)");
+        }
+        Console.WriteLine("Would you like to buy any?");
+        Console.WriteLine("1) Leather 2)Chainmail 3)Plate");
+        input = Console.ReadLine();
+        if ((input == "1") && (PlayerCharacter.Value >= Armors[0].Value))
+        {
+            PlayerCharacter.Armor = Armors[0];
+            PlayerCharacter.Value -= Weapons[0].Value;
+            Console.WriteLine("You have equiped Leather armor and have " + PlayerCharacter.Value + " gold left.");
+        }
+        else if ((input == "2") && (PlayerCharacter.Value >= Armors[1].Value))
+        {
+            PlayerCharacter.Armor = Armors[1];
+            PlayerCharacter.Value -= Weapons[0].Value;
+            Console.WriteLine("You have equiped a Chain Mail armor and have " + PlayerCharacter.Value + " gold left.");
+        }
+        else if ((input == "3") && (PlayerCharacter.Value >= Armors[2].Value))
+        {
+            PlayerCharacter.Armor = Armors[2];
+            PlayerCharacter.Value -= Weapons[0].Value;
+            Console.WriteLine("You have equiped a Plate armor and have " + PlayerCharacter.Value + " gold left.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid option or not enough gold.");
+            Shop();
+        }
+    }
+
+    // Lists and Allows Player to buy potions after checking if he has enough gold for the selected potion
+
+    else if (input == "3")
+    {
+        for (int i = 0; i < Potions.Count; i += 1)
+        {
+            Item potion = Potions[i];
+            Console.WriteLine(potion.Name + " (" + potion.Value + " gold)");
+        }
+        Console.WriteLine("Would you like to buy any?");
+        Console.WriteLine("1) Small Potion 2)Medium Potion 3)Large Potion");
+        input = Console.ReadLine();
+
+        // Checks to see if the play had enough for what they selected
+
+        if ((input == "1") && (PlayerCharacter.Value >= Potions[0].Value))
+        {
+            PlayerSmPotion =+ 1;
+            Console.WriteLine("You now have " + PlayerSmPotion + " small potions.");
+        } else if ((input == "2") && (PlayerCharacter.Value >= Potions[1].Value))
+        {
+            PlayerPotion =+ 1;
+            Console.WriteLine("You now have " + PlayerPotion + " medium potions.");
+        } else if ((input == "3") && (PlayerCharacter.Value >= Potions[3].Value))
+        {
+            PlayerLgPotion =+ 1;
+            Console.WriteLine("You now have " + PlayerLgPotion + " large potions.");
+        } else
+        {
+            Console.WriteLine("Invalid Option or not enough gold.");
+            Shop();
+        }
     }
 
 }
 
 void Inventory()
 {
-    Console.WriteLine("This is your inventory");
+    Console.WriteLine("Weapon: " + PlayerCharacter.Weapon.Name);
+    Console.WriteLine("Armor: " + PlayerCharacter.Armor.Name);
+    Console.WriteLine("Small Potions: " + PlayerSmPotion);
+    Console.WriteLine("Medium Potions: " + PlayerPotion);
+    Console.WriteLine("Large Potions: " + PlayerLgPotion);
+    Console.WriteLine("Would you like to inspect your gear or use a potion?");
+    Console.WriteLine("1) Weapon 2) Armor 3) Small Potion 4) Medium Potion 5 Large Potion");
+    input = Console.ReadLine();
+    if (input == "1")
+    {
+        Console.WriteLine($"Weapon: {PlayerCharacter.Weapon.Name}");
+        Console.WriteLine($"Damage: {PlayerCharacter.Weapon.Effect}");
+    }
+    else if (input == "2")
+    {
+        Console.WriteLine($"Armor: {PlayerCharacter.Armor.Name}");
+        Console.WriteLine($"Blocks: {PlayerCharacter.Armor.Effect}");
+    }
+    else if ((input == "3") && (PlayerSmPotion > 0))
+    {
+        Console.WriteLine($"You have used a small health potion to heal {SmallPotion.Value} Hp");
+        Console.WriteLine($"You now have {PlayerCharacter.Hp}.");
+    }
+    else if ((input == "4") && (PlayerPotion > 0))
+    {
+        Console.WriteLine($"You have used a medium health potion to heal {MediumPotion.Value} Hp");
+        Console.WriteLine($"You now have {PlayerCharacter.Hp}.");
+
+    }
+    else if ((input == "5") && (PlayerLgPotion > 0))
+    {
+        Console.WriteLine($"You have used a small health potion to heal {LargePotion.Value} Hp");
+        Console.WriteLine($"You now have {PlayerCharacter.Hp}.");
+    } 
+    else if (((input == "3") && (PlayerSmPotion <= 0)) || ((input == "4") && (PlayerPotion <= 0)) || ((input == "5") && (PlayerLgPotion <= 0)))
+    {
+        Console.WriteLine("You do not have any of those.");
+        Inventory();
+    } 
+    else {
+
+    }
 }
 
 void Info()
 {
-    Console.WriteLine("this is the information screen");
+    Console.WriteLine("Player Stats");
+    Console.WriteLine($"Player Health: {PlayerCharacter.Hp}");
+    Console.WriteLine($"Player Strength: {PlayerCharacter.Str}");
+    Console.WriteLine($"Player Damage: {PlayerCharacter.Str + PlayerCharacter.Weapon.Effect}");
+    Console.WriteLine($"Player Speed: {PlayerCharacter.Speed}");
+    Console.WriteLine($"Player Consitution: {PlayerCharacter.Con}");
+    Console.WriteLine($"Player Restance: {PlayerCharacter.Res}");
+    Console.WriteLine($"Player Gold: {PlayerCharacter.Value}");
 }
 
 while (game)
@@ -159,23 +364,35 @@ public class Character
     public string Name { get; set; }
     public int Hp { get; set; }
     public int Str { get; set; }
-    public int Dex { get; set; }
+    public int Speed { get; set; }
     public int Con { get; set; }
     public int Res { get; set; }
     public int Value { get; set; }
+    public Item Weapon { get; set; }
+    public Item Armor { get; set; }
 
-    //Character class Constructor
+    //Character Constructor
 
-    public Character(string name, int hp, int str, int dex, int con, int res, int value)
+    public Character(string name, int hp, int str, int speed, int con, int res, int value, Item weapon, Item armor)
     {
         Name = name;
         Hp = hp;
         Str = str;
-        Dex = dex;
+        Speed = speed;
         Con = con;
         Res = res;
         Value = value;
+        Weapon = weapon;
+        Armor = armor;
     }
+
+    //Character Object Cloner to make a shallow clone 
+
+    public Character Clone()
+    {
+        return (Character) this.MemberwiseClone();
+    }
+
 
     //Where we will put all of the methods of the character class.
     
@@ -183,9 +400,84 @@ public class Character
     {
         Console.WriteLine(Name + " attacked " + target.Name);
         target.Hp -= Str;
-        Console.WriteLine(target.Name + " has " + target.Hp + " health left.");
+        Console.WriteLine($"{Name} dealt {Str + Weapon.Effect} damage to {target.Name}");
     }
 
 }
 
+// Creates Item Class
+
+public class Item
+{
+    public string Type { get; set; }
+    public string Name { get; set; }
+    public int Effect { get; set; }
+    public int Value { get; set; }
+
+    // Weapon Constructor
+
+    public Item(string type, string name, int effect, int value)
+    {
+        Type = type;
+        Name = name;
+        Effect = effect;
+        Value = value;
+    }
+}
+
+//Creates Weapon Class
+
+//public class Weapon
+//{
+//public string WeaponName { get; set; }
+//public int WeaponDamage { get; set; }
+//public int WeaponValue { get; set; }
+
+// Weapon Constructor
+
+//public Weapon(string name, int damage, int value)
+//{
+//WeaponName = name;
+//WeaponDamage = damage;
+//WeaponValue = value;
+//    }
+//}
+
+//Creates Potion Class
+
+//public class Potion
+//{
+//public string PotionName { get; set; }
+//public int PotionDamage { get; set; }
+//public int PotionValue { get; set; }
+
+// Weapon Constructor
+
+//public Potion(string name, int damage, int value)
+//{
+//PotionName = name;
+//PotionDamage = damage;
+//PotionValue = value;
+//    }
+//}
+
+//Creates Armor Class
+
+//public class Armor
+//{
+//public string ArmorName { get; set; }
+//public int ArmorDamage { get; set; }
+//public int ArmorValue { get; set; }
+
+// Weapon Constructor
+
+//public Armor(string name, int damage, int value)
+//{
+//ArmorName = name;
+//ArmorDamage = damage;
+//ArmorValue = value;
+//    }
+//}
+
+// 
 
